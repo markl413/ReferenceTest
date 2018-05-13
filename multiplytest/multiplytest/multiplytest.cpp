ddea33f09404442f80d3b798e35dce13372eab18
markl413 <39225973+markl413@users.noreply.github.com>
2018-05-13 05:26:48 -0400

    Initial commit

---
+++ multiplytest/multiplytest/multiplytest.cpp
@@ -0,0 +1,9 @@
+// multiplytest.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+double __stdcall multiplyTwoNumbers(double x, double y) {
+	return x*y;
+}
+
+
